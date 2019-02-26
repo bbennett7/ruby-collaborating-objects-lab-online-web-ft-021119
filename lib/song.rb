c2b8e1dfb5c4f1f034743bdbcf628ee330bf8771
Bryn Bennett <bryn.bennett@gmail.com>
2019-02-26 03:26:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,7 +22,6 @@ class Song
     song = Song.new(song_name)
     
     artist_index = Artist.all.index{|artist| artist.name == artist_name}
-            binding.pry
     song.artist = Artist.all[artist_index]
     Artist.all[artist_index].songs << song_name 
     song
